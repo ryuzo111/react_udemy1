66b9529b992ca5d8db5aeef4fa0319d1b64844cc
ryuzo <73731762+ryuzo111@users.noreply.github.com>
2021-11-23 23:06:31 +0900

    first

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ColorfulMessage = () => {
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
+  return <p style={contentStyle}>おげんきですか</p>;
+};
+
+export default ColorfulMessage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>おげんきですか</p>
+    </>
+  );
+};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDom.render(<App />, document.getElementById("root"));
+// ここまでがリアクトの基盤
